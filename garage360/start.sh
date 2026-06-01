436900079f5e3c47355dab5227e9cfa926405fcd
efe <efeaydnfb@gmail.com>
2026-06-01 03:44:43 +0300

    .
    .

---
+++ garage360/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
+php artisan db:seed --force 2>/dev/null || true
+php artisan storage:link 2>/dev/null || true
+
+apache2-foreground
